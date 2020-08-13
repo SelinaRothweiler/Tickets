7f21bd94cbd8590a387789c8ec7fa1a14479a14e
Sel007 <selina.08225.1@gmail.com>
2020-08-13 00:02:24 -0400

    Add project files.

---
+++ Tickets/FrmTickets.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Tickets
+{
+    partial class FrmTickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTickets = new System.Windows.Forms.Button();
+            this.BtnElectricBill = new System.Windows.Forms.Button();
+            this.BtnAverage = new System.Windows.Forms.Button();
+            this.BtnGasBill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnTickets
+            // 
+            this.BtnTickets.Location = new System.Drawing.Point(73, 62);
+            this.BtnTickets.Name = "BtnTickets";
+            this.BtnTickets.Size = new System.Drawing.Size(227, 98);
+            this.BtnTickets.TabIndex = 0;
+            this.BtnTickets.Text = "Get Ticket Price";
+            this.BtnTickets.UseVisualStyleBackColor = true;
+            this.BtnTickets.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnElectricBill
+            // 
+            this.BtnElectricBill.Location = new System.Drawing.Point(404, 250);
+            this.BtnElectricBill.Name = "BtnElectricBill";
+            this.BtnElectricBill.Size = new System.Drawing.Size(237, 87);
+            this.BtnElectricBill.TabIndex = 1;
+            this.BtnElectricBill.Text = "Electric Bill";
+            this.BtnElectricBill.UseVisualStyleBackColor = true;
+            this.BtnElectricBill.Click += new System.EventHandler(this.BtnElectricBill_Click);
+            // 
+            // BtnAverage
+            // 
+            this.BtnAverage.Location = new System.Drawing.Point(404, 62);
+            this.BtnAverage.Name = "BtnAverage";
+            this.BtnAverage.Size = new System.Drawing.Size(237, 98);
+            this.BtnAverage.TabIndex = 2;
+            this.BtnAverage.Text = "Average";
+            this.BtnAverage.UseVisualStyleBackColor = true;
+            this.BtnAverage.Click += new System.EventHandler(this.BtnAverage_Click);
+            // 
+            // BtnGasBill
+            // 
+            this.BtnGasBill.Location = new System.Drawing.Point(73, 250);
+            this.BtnGasBill.Name = "BtnGasBill";
+            this.BtnGasBill.Size = new System.Drawing.Size(227, 87);
+            this.BtnGasBill.TabIndex = 3;
+            this.BtnGasBill.Text = "Gas Bill";
+            this.BtnGasBill.UseVisualStyleBackColor = true;
+            this.BtnGasBill.Click += new System.EventHandler(this.BtnGasBill_Click);
+            // 
+            // FrmTickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnGasBill);
+            this.Controls.Add(this.BtnAverage);
+            this.Controls.Add(this.BtnElectricBill);
+            this.Controls.Add(this.BtnTickets);
+            this.Name = "FrmTickets";
+            this.Text = "Tickets";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTickets;
+        private System.Windows.Forms.Button BtnElectricBill;
+        private System.Windows.Forms.Button BtnAverage;
+        private System.Windows.Forms.Button BtnGasBill;
+    }
+}
+
---
+++ Tickets/FrmTickets.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tickets
+{
+    public partial class FrmTickets : Form
+    {
+        public FrmTickets()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age;
+            double price = 0;
+
+            age = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter you age ", "Tickets", ""));
+
+            if(age <= 12)
+            {
+                price = 5.00;
+            }
+            else if (age >= 13 && age <= 20)
+            {
+                price = 6.00;
+            }
+            else if (age >= 21 && age <= 64)
+            {
+                price = 10.00;
+            }
+            else if (age >=65)
+            {
+                price = 2.00;
+            }
+
+            MessageBox.Show("Your price is " + price.ToString("c"));
+        }
+
+
+        private void BtnAverage_Click(object sender, EventArgs e)
+        {
+            int mark1;
+            int mark2;
+            int mark3;
+            int mark4;
+            double average;
+
+            mark1 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter you mark ", "Average", ""));
+            mark2 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter you mark ", "Average", ""));
+            mark3 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter you mark ", "Average", ""));
+            mark4 = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter you mark ", "Average", ""));
+
+            average = (double)(mark1 + mark2 + mark3 + mark4) / 4;
+
+            MessageBox.Show("Your average is " + average.ToString("n2"));
+
+            if (average < 50)
+            {
+                MessageBox.Show("You failed :(");
+            }
+            else if (average >= 50)
+            {
+                MessageBox.Show("Yay...You Passed!");
+            }
+
+        }
+
+        private void BtnGasBill_Click(object sender, EventArgs e)
+        {
+            int gas;
+            double bill = 0;
+
+            gas = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter your gas usage in ccf ", "Gas Bill", ""));
+
+            if (gas <= 8)
+            {
+                bill = gas * .50;
+            }
+            else if (gas > 8)
+            {
+                bill = gas * .50 + (gas - 8) * .30;
+            }
+
+            MessageBox.Show("Here is your total gas bill. " + bill.ToString("c"));
+        }
+        private void BtnElectricBill_Click(object sender, EventArgs e)
+        {
+            int kwh;
+            double bill = 0;
+
+            kwh = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter your electric usage in kwh ", "Electric Bill", ""));
+
+            if (kwh <= 12)
+            {
+                bill = 2.80;
+            }
+            else if (kwh > 12 && kwh <= 78)
+            {
+                bill = 2.80 + (kwh - 12) * .08;
+            }
+            else if(kwh >= 90)
+            {
+                bill = 2.80 + (78 * .08) + (kwh - 90) * .06;
+            }
+
+            MessageBox.Show("Here is your electric bill " + bill.ToString("c"));
+        }
+    }
+}
